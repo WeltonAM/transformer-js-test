e3d7bc24b217f1e208369e7634257a181d7d19d3
Welton Almeida de Matos <wamatos7@gmail.com>
2026-01-07 14:00:24 -0300

    Update main-router.tsx
    - Fazer que a rota '/' redirecione para '/translator'

--- src/router/main-router.tsx
+++ src/router/main-router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import TranslatorPage from "../pages/translator/translator-page";
 import { TranslatorWorkerProvider } from "../data/contexts/translator-context";
 import { ATSWorkerProvider } from "../data/contexts/ats-analyzer-context";
@@ -7,6 +7,8 @@ import ATSPage from "../pages/ats-analyzer/ats-page";
 export function MainRouter() {
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/translator" replace />} />
+
             <Route
                 path="/translator"
                 element={
